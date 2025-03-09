a4905e383eff38c52ae10a84ec77f9aadd482de1
jan438 <jan438@users.noreply.github.com>
2025-03-09 14:51:38 +0100

    a4 format

--- Olieslager.py
+++ Olieslager.py
@@ -55,7 +55,7 @@ for i in range(len(kamers)):
     print(kamers[i].bewoner)
     
 
-d = Drawing(400, 200)
+d = Drawing(595, 842)
 d.add(Rect(50, 50, 300, 100, fillColor=colors.yellow))
 d.add(String(150,100, 'Hello World', fontSize=18, fillColor=colors.red))
 d.add(String(180,86, 'Special characters \\\\xc2\\xa2\\xc2\\xa9\\xc2\\xae\\xc2\\xa3\\xce\\xb1\\xce\\xb2', fillColor=colors.red))
